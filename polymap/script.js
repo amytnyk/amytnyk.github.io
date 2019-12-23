f2dc11ea9e9286fb9784641477e341bfd50526a7
Alex Mytnyk <alex.mytnyk@ukr.net>
2019-12-23 14:01:46 +0200

    beta

--- polymap/script.js
+++ polymap/script.js
@@ -14,6 +14,42 @@ const Field = {
     OCEAN: 3
 }
 
+function downloadObjectAsJson(exportObj, exportName){
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    let downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+
+function downloadMapAsImage(exportName){
+    let downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     canvas.toDataURL('image/png'));
+    downloadAnchorNode.setAttribute("download", exportName + ".png");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+
+function resize() {
+    if (window.innerWidth > 800) {
+        document.getElementById("canvas").setAttribute("width", window.innerWidth / 2);
+        document.getElementById("canvas").setAttribute("height", window.innerWidth / 2);
+    } else {
+        document.getElementById("canvas").setAttribute("width", window.innerWidth);
+        document.getElementById("canvas").setAttribute("height", window.innerWidth);
+    }
+}
+
+window.onresize = function(event) {
+    resize();
+    update();
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
@@ -29,6 +65,52 @@ document.addEventListener("DOMContentLoaded", () => {
         if (isMouseDown)
             clickedOnField(e);
     });
+    document.querySelector(".load_from_file").onclick = () => {
+        document.querySelector("#file").click();
+    };
+    document.querySelector("#file").onchange = function() {
+        if (document.getElementById("file").files[0] == undefined) {
+            alert("Select file");
+            return;
+        }
+        let f = new FileReader();
+        f.onload = function() {
+            map = JSON.parse(this.result);
+            update();
+        };
+        f.readAsText(document.getElementById("file").files[0]);
+        document.querySelector("#file").value = "";
+    };
+    document.querySelector(".save_to_file").onclick = () => {
+        downloadObjectAsJson(map, "map");
+    };
+    document.querySelector(".save_as_image").onclick = () => {
+        downloadMapAsImage("map");
+    };
+    document.querySelector(".clear_map").onclick = () => {
+        initMap();
+        update();
+    };
+    document.querySelector(".select_ground").onclick = () => {
+        current_brush = Field.GROUND;
+        document.querySelector(".select_ground").setAttribute("checked", "checked");
+        document.querySelector(".select_water").removeAttribute("checked");
+        document.querySelector(".select_unknown").removeAttribute("checked");
+    };
+    document.querySelector(".select_water").onclick = () => {
+        current_brush = Field.SEA;
+        document.querySelector(".select_water").setAttribute("checked", "checked");
+        document.querySelector(".select_ground").removeAttribute("checked");
+        document.querySelector(".select_unknown").removeAttribute("checked");
+    };
+    document.querySelector(".select_unknown").onclick = () => {
+        current_brush = Field.UNKNOWN;
+        document.querySelector(".select_unknown").setAttribute("checked", "checked");
+        document.querySelector(".select_water").removeAttribute("checked");
+        document.querySelector(".select_ground").removeAttribute("checked");
+    };
+
+    resize();
     initMap();
     update();
 });
@@ -38,8 +120,8 @@ function initMap() {
 }
 
 function calculateSize() {
-    let width_per_field = Math.ceil(window.innerWidth / 10 * 6) / width;
-    let height_per_field = 600 / height;
+    let width_per_field = document.getElementById("canvas").offsetWidth / width;
+    let height_per_field = document.getElementById("canvas").offsetHeight / height;
     field_size = Math.min(width_per_field, height_per_field);
 }
 
@@ -90,8 +172,6 @@ function drawAll() {
     drawGrid();
 }
 
-
-
 function update() {
     calculateSize();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -105,8 +185,10 @@ function clickedOnField(e) {
 
     let fieldX = Math.ceil(mouseX / field_size) - 1;
     let fieldY = Math.ceil(mouseY / field_size) - 1;
-
-    map[fieldY][fieldX] = current_brush;
-    drawPart(fieldX, fieldY);
-    drawGrid();
+    
+    if (fieldY >= 0 && fieldY < height && fieldX >= 0 && fieldX < width) {
+        map[fieldY][fieldX] = current_brush;
+        drawPart(fieldX, fieldY);
+        drawGrid();
+    }
 }
