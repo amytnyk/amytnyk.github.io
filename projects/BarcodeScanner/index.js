6969a1f450bd992bb84dbbc746cf4f3be5a807ef
amytnyk <alex.mytnyk@ukr.net>
2020-06-26 22:09:11 +0300

    barcode

---
+++ projects/BarcodeScanner/index.js
@@ -0,0 +1,8 @@
+let videoElement = null; 
+
+document.addEventListener("DOMContentLoaded", () => {
+  videoElement = document.querySelector('video');
+  initCamera(videoElement, (barcode) => {
+    console.log(barcode);
+  });
+});
