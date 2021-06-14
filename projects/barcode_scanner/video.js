6fe482454353025c212d3608a78640baaecd806d
amytnyk <alex.mytnyk@ukr.net>
2021-06-14 14:21:37 +0300

    barcode

--- projects/barcode_scanner/video.js
+++ projects/barcode_scanner/video.js
@@ -66,7 +66,7 @@ let initCamera = function(videoElement, onBarcodeDetected) {
   
     const constraints = {
       video: {
-        facingMode: { exact: window.videoCheck() ? "environment" : "user" },
+        facingMode: { exact: window.mobileCheck() ? "environment" : "user" },
         width: { min: 1280, ideal: 1920 }, height: { min: 720, ideal: 1080 }
       }
     };
